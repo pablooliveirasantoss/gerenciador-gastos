eac9eaa280685584983d2e170551eb449deee15d
pablooliveirasantos <pablooliveirasantos1347@gmail.com>
2026-06-02 09:52:19 -0300

    Adiciona arquivos do projeto

---
+++ index.js
@@ -0,0 +1,16 @@
+let pessoa = 'pablo'
+
+const gastoMensal = [400, 100, 900]
+
+let gastostotais = gastoMensal[0] + gastoMensal[1] + gastoMensal[2]
+
+let hora = new Date()
+console.log(Math.max(400, 100, 900))
+console.log(`nome ${pessoa}
+    o gasto mensal: ${gastoMensal}
+    maior gasto: ${Math.max(400, 100, 900)}
+    valor total: ${gastostotais} 
+    hora: ${hora}
+    
+
+    `)
